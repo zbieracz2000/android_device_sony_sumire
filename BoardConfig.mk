a4fffbe8c7f97bb4262f4e6c70e9b876912da93d
zbieracz2000 <zbieracz2000@interia.pl>
2023-05-07 15:38:59 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/kitakami-common/PlatformConfigOmni.mk
-
 TARGET_BOOTLOADER_BOARD_NAME := E6653
 
 WIFI_BUS := PCIE
